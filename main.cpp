c03a8f4a2928344eb6b7f2d0cef298e5bdbf0fcb
Sierra Brandt <44011600+SierraBrandt@users.noreply.github.com>
2021-09-07 19:39:50 +0000

    Added Code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+//Calulates charge of Uber ride
+//Author: Sierra Brandt
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  double airportFee = 2.51, upFrontCharge = 3.72, uberCharge = 19.46,
+  tip = 0.08 * (upFrontCharge + uberCharge), total = airportFee + upFrontCharge + uberCharge + tip;
+
+  cout << "Airport Fee: " << airportFee << "\n";
+  cout << "Up Front Charge : " << upFrontCharge << "\n";
+  cout << "Uber Charge: " << uberCharge << "\n";
+  cout << "Tip: " << tip << "\n";
+  cout << "Total: " << total << "\n";
+
 }
